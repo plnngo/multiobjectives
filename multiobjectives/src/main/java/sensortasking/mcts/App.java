26039cddb3319b7c42707fc3e531606c96cdfe24
Linh Ngo <p.l.n.ngo@tudelft.nl>
2024-03-19 14:41:14 +0100

    First commit

---
+++ multiobjectives/src/main/java/sensortasking/mcts/App.java
@@ -0,0 +1,17 @@
+package sensortasking.mcts;
+
+import org.orekit.time.AbsoluteDate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        AbsoluteDate date = new AbsoluteDate();
+        System.out.println(date);
+    }
+}
