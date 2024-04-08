402d91fddc3a6275eac6481cf70fe3f5a5e72458
Linh Ngo <p.l.n.ngo@tudelft.nl>
2024-04-08 16:42:45 +0200

    Add test to check if query of omms works

--- multiobjectives/src/test/java/data/SpaceTrackLoaderTest.java
+++ multiobjectives/src/test/java/data/SpaceTrackLoaderTest.java
@@ -1,13 +1,20 @@
 package data;
 
+import static org.junit.Assert.assertThat;
+
 import java.io.File;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.orekit.data.DataContext;
 import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
 
+import junit.framework.Assert;
+
 public class SpaceTrackLoaderTest {
 
+    @Before
     public void init() {
 
         // Load orekit data
@@ -17,6 +24,31 @@ public class SpaceTrackLoaderTest {
         DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
         manager.addProvider(new DirectoryCrawler(orekitData));
     }
+
+    @Test
+    public void testBuildOmmQueryLimitNoradIdAndEpoch(){
+
+        // Set input of the query
+        int[] noradId = new int[]{25544, 38101};
+        boolean sortByEpoch = true;
+        String limitInc = "";
+        String limitEpoch = ">now-30";
+
+        String actualQuery = 
+            SpaceTrackLoader.buildOmmQuery(noradId, sortByEpoch, null, null, 
+                                           limitInc, null, null, null, limitEpoch);
+
+        // Build expected query string
+        String expectedQuery = "/basicspacedata/query/class/gp" +  "/NORAD_CAT_ID/" 
+                                + Integer.toString(noradId[0]) + "," + Integer.toString(noradId[1])
+                                + "/orderby/" + "EPOCH%20ASC"
+                                + "/EPOCH/" + limitEpoch 
+                                + "/format/xml";
+
+        // Assert
+        org.junit.Assert.assertEquals(expectedQuery, actualQuery);
+        
+    } 
 
     
 }
