e9ca6edefc417ad743c600309004f9ea9546bdd7
Linh Ngo <p.l.n.ngo@tudelft.nl>
2024-05-21 18:27:10 +0200

    Set FOR detector with ISS as example including covariance propagation.

--- multiobjectives/src/main/java/sensortasking/mcts/App.java
+++ multiobjectives/src/main/java/sensortasking/mcts/App.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.Locale;
 
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
+import org.hipparchus.linear.DiagonalMatrix;
+import org.hipparchus.linear.RealMatrix;
 import org.hipparchus.ode.events.Action;
 import org.hipparchus.util.FastMath;
 import org.orekit.bodies.BodyShape;
@@ -16,11 +18,18 @@ import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.frames.TopocentricFrame;
 import org.orekit.models.earth.ReferenceEllipsoid;
+import org.orekit.orbits.OrbitType;
+import org.orekit.orbits.PositionAngle;
+import org.orekit.propagation.MatricesHarvester;
 import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.StateCovariance;
+import org.orekit.propagation.StateCovarianceMatrixProvider;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
 import org.orekit.propagation.events.ElevationDetector;
 import org.orekit.propagation.events.EventDetector;
+import org.orekit.propagation.events.EventsLogger;
+import org.orekit.propagation.events.EventsLogger.LoggedEvent;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
@@ -41,8 +50,8 @@ public class App {
             manager.addProvider(new DirectoryCrawler(orekitData));
 
             // Test TLE
-            TLE iss = new TLE("1 25544U 98067A   24138.42237498  .00027365  00000-0  46131-3 0  9998",
-                            "2 25544  51.6391 107.8732 0003463 176.7237 327.9829 15.51459620453778");
+            TLE iss = new TLE("1 25544U 98067A   24142.35003124  .00025335  00000-0  42455-3 0  9992",
+                            "2 25544  51.6384  88.3676 0003163 192.6310 305.1222 15.51672766454387");
 
             //  Initial state definition : date, orbit
             final TimeScale utc = TimeScalesFactory.getUTC();
@@ -70,10 +79,22 @@ public class App {
             final GeodeticPoint station1 = new GeodeticPoint(latitude, longitude, altitude);
             final TopocentricFrame sta1Frame = new TopocentricFrame(earth, station1, "station1");
 
+            // Covariance
+            final RealMatrix covInitMatrix = new DiagonalMatrix(new double[]{10., 10., 10., 100., 100., 100.});
+            final StateCovariance covInit = new StateCovariance(covInitMatrix, initialDate, FramesFactory.getTEME(), OrbitType.CARTESIAN, PositionAngle.MEAN);
+            final String stmAdditionalName = "stm";
+            final MatricesHarvester harvester = tleProp.setupMatricesComputation(stmAdditionalName, null, null);
+
+            final StateCovarianceMatrixProvider provider = new StateCovarianceMatrixProvider("covariance", stmAdditionalName,
+                                                                                         harvester, OrbitType.CARTESIAN, PositionAngle.MEAN, covInit);
+
+            tleProp.addAdditionalStateProvider(provider);
+
             // Event definition
             final double maxcheck  = 60.0;
             final double threshold =  0.001;
             final double elevation = FastMath.toRadians(5.0);
+            
             final EventDetector sta1Visi =
                     new ElevationDetector(maxcheck, threshold, sta1Frame).
                     withConstantElevation(elevation).
@@ -82,16 +103,34 @@ public class App {
                                             ((ElevationDetector) d).getTopocentricFrame().getName() +
                                            (increasing ? " begins at " : " ends at ") +
                                            s.getDate().toStringWithoutUtcOffset(utc, 3));
+                        
                         return increasing ? Action.CONTINUE : Action.STOP;
                     });
 
             // Add event to be detected
-            tleProp.addEventDetector(sta1Visi);
+            final EventsLogger logger = new EventsLogger();
+            tleProp.addEventDetector(logger.monitorDetector(sta1Visi));
 
             // Propagate from the initial date to the first raising or for the fixed duration
-            final SpacecraftState finalState = tleProp.propagate(initialDate.shiftedBy(60.*60.*48));
+            AbsoluteDate targetEpoch = initialDate.shiftedBy(60.*60.*24);
+            final SpacecraftState finalState = tleProp.propagate(targetEpoch);
 
             System.out.println(" Final state duration from initial date (s) : " + finalState.getDate().durationFrom(initialDate));
+            
+
+             for (LoggedEvent le : logger.getLoggedEvents()) {
+            if (le.isIncreasing()) {
+                System.out.println("Enter FOR " + le.getDate());
+            } else {
+                System.out.println("Exit FOR " + le.getDate());;
+            }
+            System.out.println("Propagate the covariance to " + le.getDate().toString() + " ...");
+            final RealMatrix covProp = provider.getStateCovariance(le.getState()).getMatrix();
+            System.out.println(covProp.toString());
+        }
+            
+
+
 
         } catch (OrekitException oe) {
             System.err.println(oe.getLocalizedMessage());
