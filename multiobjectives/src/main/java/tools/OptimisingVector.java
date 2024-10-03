76bd95790dafadf9334b6d84a31de60c25cd3e46
Linh Ngo <plnngo@tudelft.nl>
2024-10-03 14:57:58 +0200

    Add test to extract dominating solutions

--- multiobjectives/src/main/java/tools/OptimisingVector.java
+++ multiobjectives/src/main/java/tools/OptimisingVector.java
@@ -1,42 +1,97 @@
 package tools;
 
-import java.util.List;
-
 import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 
 @Getter
 public class OptimisingVector {
 
     protected List<double[]> all;
 
-    public OptimisingVector(List<double[]> vectors) {
-        double[][] allVec = new double[vectors.size()][vectors.get(0).length];
+    public OptimisingVector(List<double[]> vectors, int dim) {
+        if(vectors.size()>0) {
+            double[][] allVec = new double[vectors.size()][vectors.get(0).length];
 
-        // Fill multi dimensional array with vector entries
-        for(int i=0; i<vectors.size(); i++){
-            double[] vec = vectors.get(i);
-            for(int j=0; j<vec.length; j++) {
-                allVec[i][j] = vec[j]; 
+            // Fill multi dimensional array with vector entries
+            for(int i=0; i<vectors.size(); i++){
+                double[] vec = vectors.get(i);
+                for(int j=0; j<vec.length; j++) {
+                    allVec[i][j] = vec[j]; 
+                }
             }
+
+            // Sort array by first entries of vector
+            Arrays.sort(allVec, new Comparator<double[]>() {
+
+                @Override
+                public int compare(double[] o1, double[] o2) {
+                    Double d1 = o1[dim];
+                    Double d2 = o2[dim];
+                    return d1.compareTo(d2);
+                }
+            });
+
+            List<double[]> sorted = Arrays.asList(allVec);
+            this.all = sorted;
         }
-
-        // Sort array by first entries of vector
-        Arrays.sort(allVec, new Comparator<double[]>() {
-
-            @Override
-            public int compare(double[] o1, double[] o2) {
-                Double d1 = o1[0];
-                Double d2 = o2[0];
-                return d1.compareTo(d2);
-            }
-        });
-
-        List<double[]> sorted = Arrays.asList(allVec);
-        this.all = sorted;
+        else {
+            this.all = new ArrayList<double[]>();
+        }
     }
 
+    public void addVec(double[] toAdd) {
+
+        // Add new vector in the right order to the vector sequences according to 1st entry
+        for(int i=0; i<this.all.size(); i++) {
+            if(all.get(i)[0]>toAdd[0]) {
+                this.all.add(i, toAdd);
+                break;
+            } else if(i==this.all.size()-1) {
+                this.all.add(toAdd);
+            }
+        }
+    }
+    public List<double[]> getDominatingVecs(double[] toCompare, boolean[] order, int dim) {
+
+        if(toCompare.length != order.length) {
+            throw new IllegalArgumentException("Vector and order instructions" 
+                                                + " do not have the same dimensions");
+        } 
+        if (dim == toCompare.length) {
+            return this.all;
+        } else {
+
+            // Order vector of sequence according to instruction
+            List<double[]> dominating = new ArrayList<double[]>();
+            if(order[dim]) {
+                // descending order = vector entry dominates by maximising
+                for(int entry=0; entry<this.all.size(); entry++) {
+                    int endIndex = this.all.size() - 1;
+                    if(this.all.get(endIndex - entry)[dim] > toCompare[dim]) {
+                        dominating.add(this.all.get(endIndex - entry));
+                    } else {
+                        break;
+                    }
+                }
+                
+            } else {
+                // ascending order = vector entry dominates by minimising
+                for(int entry=0; entry<this.all.size(); entry++) {
+                    if (this.all.get(entry)[dim] < toCompare[dim]) {
+                        dominating.add(this.all.get(entry));
+                    } else {
+                        break;
+                    }
+                }
+            }
+            dim++;
+            OptimisingVector ov = new OptimisingVector(dominating, dim);
+            return ov.getDominatingVecs(toCompare, order, dim);
+        } 
+    }
 
 }
--- multiobjectives/src/test/java/tools/OptimisingVectorTest.java
+++ multiobjectives/src/test/java/tools/OptimisingVectorTest.java
@@ -12,6 +12,8 @@ import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
 
 public class OptimisingVectorTest {
+
+    public List<double[]> vectors = new ArrayList<double[]>();
     
     @Before
     public void init() {
@@ -22,29 +24,41 @@ public class OptimisingVectorTest {
         File orekitData = new File(workingDir + orekitDataDir);
         DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
         manager.addProvider(new DirectoryCrawler(orekitData));
+
+        // set input 
+        this.vectors.add(new double[]{8, 9, 2});
+        this.vectors.add(new double[]{9, 4, 2});
+        this.vectors.add(new double[]{5, 3, 2});
+        this.vectors.add(new double[]{10, 7, 3});
+        this.vectors.add(new double[]{9, 7, 5});
+        this.vectors.add(new double[]{4, 3, 5});
+        this.vectors.add(new double[]{9, 8, 3});
+        this.vectors.add(new double[]{1, 7, 10});
+        this.vectors.add(new double[]{4, 4, 5});
+        this.vectors.add(new double[]{1, 9, 5});
     }
 
     @Test
     public void testSortingByFirstEntry() {
-
-        // Set input
-        List<double[]> vectors = new ArrayList<double[]>(); 
-        vectors.add(new double[]{8, 9, 2});
-        vectors.add(new double[]{9, 4, 2});
-        vectors.add(new double[]{5, 3, 2});
-        vectors.add(new double[]{10, 7, 3});
-        vectors.add(new double[]{9, 7, 5});
-        vectors.add(new double[]{4, 3, 5});
-        vectors.add(new double[]{9, 8, 3});
-        vectors.add(new double[]{1, 7, 10});
-        vectors.add(new double[]{4, 4, 5});
-        vectors.add(new double[]{1, 9, 5});
-
-        OptimisingVector test = new OptimisingVector(vectors);
+       
+        OptimisingVector test = new OptimisingVector(this.vectors, 0);
         double[] firstActual = test.getAll().get(0);
         for(int i=0; i<firstActual.length; i++){
             Assert.assertEquals(vectors.get(7)[i], firstActual[i], 1e-16);
         }
+    }
 
+    @Test
+    public void testGetDominatingVecs(){
+        OptimisingVector test = new OptimisingVector(this.vectors, 0);
+        List<double[]> out = 
+            test.getDominatingVecs(new double[]{7, 8, 4}, new boolean[]{false, false, false}, 0);
+        Assert.assertEquals(1, out.size());
+        for(int i=0; i<out.get(0).length; i++) {
+            Assert.assertEquals(vectors.get(2)[i], out.get(0)[i], 1e-16);
+        }
+
+        out = test.getDominatingVecs(new double[]{7, 8, 4}, new boolean[]{true, true, true}, 0);
+        Assert.assertEquals(0, out.size());
     }
 }
