8269482304f5b88b7e42fdd86dab749152b408d0
Linh Ngo <plnngo@tudelft.nl>
2025-03-22 13:28:55 +0100

    Set up stripe scanning simulation

---
+++ multiobjectives/src/test/java/sensortasking/stripescanning/Simulation.java
@@ -0,0 +1,21 @@
+package sensortasking.stripescanning;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.orekit.data.DataContext;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.data.DirectoryCrawler;
+
+public class Simulation {
+    @Before
+    public void init() {
+
+        // Load orekit data
+        String workingDir = System.getProperty("user.dir");
+        String orekitDataDir = "\\src\\test\\java\\resources\\orekit-data";
+        File orekitData = new File(workingDir + orekitDataDir);
+        DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
+        manager.addProvider(new DirectoryCrawler(orekitData));
+    }
+}
