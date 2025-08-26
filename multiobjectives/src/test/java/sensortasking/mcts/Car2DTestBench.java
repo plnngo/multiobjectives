2520b43184eb405a96d8f003520ede3240f5611b
plnngo <p.l.n.ngo@tudelft.nl>
2025-08-26 15:00:33 +0200

    Print results in evaulation function into csv file

--- multiobjectives/src/test/java/sensortasking/mcts/Car2DTestBench.java
+++ multiobjectives/src/test/java/sensortasking/mcts/Car2DTestBench.java
@@ -1,9 +1,12 @@
 package sensortasking.mcts;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import org.hipparchus.linear.DiagonalMatrix;
 import org.hipparchus.util.FastMath;
@@ -16,6 +19,8 @@ import org.orekit.data.DirectoryCrawler;
 import org.orekit.time.AbsoluteDate;
 
 import benchtest.Car;
+import benchtest.CarTrackingObjective;
+import benchtest.Filter;
 
 public class Car2DTestBench {
     @Before
@@ -29,19 +34,25 @@ public class Car2DTestBench {
     }
 
     @Test
-    public void testBench(){
+    public void testBench() throws IOException{
         /* Filter estimateCarA = new Filter();
         estimateCarA.run_ckf(init, P0.getData(), 5., 25.02);  */ 
 
         // Initialise first car 
-        double[] initA = new double[]{0, -1, 5, 0};
-        DiagonalMatrix P0A = new DiagonalMatrix(new double[]{0.1, 0.1, 0.01, 0.01});
+        //double[] initA = new double[]{0, -1, 5, 0};
+        double[] initA = new double[]{-2, 0, 0., -FastMath.PI/90.};
+        DiagonalMatrix P0A = new DiagonalMatrix(new double[]{0.5, 0.5, 0.05, 0.05});
+        //DiagonalMatrix P0A = new DiagonalMatrix(new double[]{0.1, 0.1, 0.01, 0.01});
+
         Car carA = new Car('A', initA, P0A.getData(), 0.);
 
         // Initialise second car
-        double[] initB = new double[]{0, 1, 5, 0};
-        //DiagonalMatrix P0B = new DiagonalMatrix(new double[]{0.1, 0.1, 0.01, 0.01});
+        //double[] initB = new double[]{0, 1, 5, 0};
+        //double[] initB = new double[]{0, 2, -FastMath.PI/180., 0};
+        double[] initB = new double[]{2, 0, 0., FastMath.PI/90.};
         DiagonalMatrix P0B = new DiagonalMatrix(new double[]{0.1, 0.1, 0.01, 0.01});
+        //DiagonalMatrix P0B = new DiagonalMatrix(new double[]{0.5, 0.5, 0.05, 0.05});
+
         Car carB = new Car('B', initB, P0B.getData(), 0.);
 
         List<ObservedObject> cars = new ArrayList<ObservedObject>();
@@ -56,7 +67,7 @@ public class Car2DTestBench {
         GeodeticPoint pos = new GeodeticPoint(FastMath.toRadians(6.),   // Geodetic latitude
                                               FastMath.toRadians(-37.),   // Longitude
                                               0.);              // in [m]
-        Fov fov = new Fov(Fov.Type.RECTANGULAR, FastMath.toRadians(2.), FastMath.toRadians(2.));
+        Fov fov = new Fov(Fov.Type.RECTANGULAR, FastMath.toRadians(0.25), FastMath.toRadians(0.25));
         Sensor sensor = new Sensor("Origin", fov, pos, 8., 7., 
                                     FastMath.toRadians(1.)/1., 7., FastMath.toRadians(5.));
 
@@ -64,8 +75,64 @@ public class Car2DTestBench {
         List<String> objectives = new ArrayList<String>(Arrays.asList( "TRACK_CAR"));
         MultiObjectiveMcts mcts = 
             new MultiObjectiveMcts(root, objectives, root.getEpoch(), 
-                                   root.getEpoch().shiftedBy(6. * 60.), "Origin", cars, 
+                                   root.getEpoch().shiftedBy(60. * 60.), "Origin", cars, 
                                    null, sensor);
-        mcts.run(500, 2);
+        List<Node> strategy = mcts.run(3000);
+
+        evaluation(strategy);
+    }
+
+    private void evaluation(List<Node> strategy) throws IOException {
+        try (FileWriter writer = new FileWriter("Strategy_Car.csv")) {
+            writer.append("car,time,meas,x1,x2,x3,x4,std1,std2,std3,std4\n");
+            char id = 'o';
+            double noisyAngle = Double.MIN_VALUE;
+            for (Node current : strategy) {
+                if (current.getClass().getSimpleName().equals("ChanceNode")) {
+                    id = ((CarTrackingObjective)((ChanceNode) current).getMacro()).getLastUpdated();
+                    AngularDirection task = ((ChanceNode) current).getMicro();
+                    Random r = new java.util.Random();
+                    noisyAngle = r.nextGaussian() * FastMath.sqrt(Filter.Rk) + task.getAngle1();
+                } else {
+                    List<ObservedObject> targets = 
+                        ((DecisionNode)current).getEnvironment().getStateTracking();
+                    for (ObservedObject target : targets) {
+                        Car car = (Car)target;
+                        if (car.getIdentifier() == id) {
+                            double[] state = car.getStateArray();
+                            double[][] cov = car.getCov();
+                            // standard deviation
+                            double[] std = new double[cov.length];
+                            for (int i=0; i<cov.length; i++) {
+                                std[i] = FastMath.sqrt(cov[i][i]);
+                            }
+
+                            writer.append(id);
+                            writer.append(",");
+                            writer.append(Double.toString(car.getTime()));
+                            writer.append(",");
+                            writer.append(Double.toString(noisyAngle));
+                            writer.append(",");
+
+
+                            // Write state vector
+                            for (int j = 0; j < state.length; j++) {
+                                writer.append(Double.toString(state[j]));
+                                writer.append(",");
+                            }
+
+                            // Write std vector
+                            for (int j = 0; j < std.length; j++) {
+                                writer.append(Double.toString(std[j]));
+                                if (j < std.length - 1) {
+                                    writer.append(",");
+                                }
+                            }
+                            writer.append("\n");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
