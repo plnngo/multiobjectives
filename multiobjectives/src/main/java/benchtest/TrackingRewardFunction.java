3d483ea1ade577341a6502bab30cb465295e0a05
plnngo <p.l.n.ngo@tudelft.nl>
2025-10-06 13:11:24 +0200

    Add java doc

--- multiobjectives/src/main/java/benchtest/TrackingRewardFunction.java
+++ multiobjectives/src/main/java/benchtest/TrackingRewardFunction.java
@@ -5,8 +5,21 @@ import sensortasking.mcts.Sensor;
 
 public abstract class TrackingRewardFunction<T> {
 
+    /**
+     * 
+     * @param last          Last (simulated) node.
+     * @param initial       Root node.
+     * @param tCampaign     Duration of observation campaign
+     * @return              Reward with respect to end of time horizon.
+     */
     public abstract double computeRewardWrtSimEnd(DecisionNode last, 
                                                   DecisionNode initial, double tCampaign);
+    
+    /**
+     * 
+     * @param current
+     * @return
+     */
     public abstract double computeImmediateReward(DecisionNode current);    
     public abstract double computeRegretWrtFov(DecisionNode lastDecision, double tobs, 
                                                Sensor sensor);
