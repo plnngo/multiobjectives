36c8f2c74b7160b27438624010af3b8984b78a40
plnngo <p.l.n.ngo@tudelft.nl>
2025-10-19 18:21:58 +0200

    Implement computation to compute chances to detect something

--- multiobjectives/src/main/java/sensortasking/mcts/Fov.java
+++ multiobjectives/src/main/java/sensortasking/mcts/Fov.java
@@ -1,6 +1,13 @@
 package sensortasking.mcts;
 
+import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.hipparchus.util.FastMath;
+import org.orekit.frames.Frame;
+import org.orekit.frames.FramesFactory;
+import org.orekit.frames.TopocentricFrame;
+import org.orekit.frames.Transform;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.utils.PVCoordinates;
 
 import lombok.Getter;
 
@@ -82,7 +89,6 @@ public class Fov {
             computeUnitVector(azMax, elMax),
             computeUnitVector(azMin, elMax)
         };
-
     }
 
     /** Field of view type. */
@@ -106,5 +112,122 @@ public class Fov {
         double y = FastMath.cos(el) * FastMath.sin(az);
         double z = FastMath.sin(el);
         return new double[] {x, y, z};
+    }
+
+    /**
+     * Compute shadow-based reward for a FoV cell, taking into account
+     * the actual sensor position above the Earth.
+     *
+     * @param sensorPV          PVCoordinates of the sensor in ECI frame
+     * @param cellDir           AngularDirection (azimuth/elevation) of the FoV cell in the 
+     *                          sensor's local frame
+     * @param date              AbsoluteDate for Sun position
+     * @param rMin              Minimum range along the LOS [m]
+     * @param rMax              Maximum range along the LOS [m]
+     * @param nBins             Number of range bins
+     * @param sensorToInertial  Transform from sensor local frame to ECI frame
+     * 
+     * @return                  Fraction of range bins NOT in Earth shadow (0–1)
+     */
+    protected static double computeShadowRewardFromSensor(PVCoordinates sensorPV,
+                                                          AngularDirection cellDir,
+                                                          AbsoluteDate date,
+                                                          double rMin, double rMax,
+                                                          int nBins,
+                                                          Transform sensorToInertial) {
+
+        Frame eci = FramesFactory.getEME2000();
+
+        // Get the LOS unit vector in the sensor frame and transform it into ECI
+        Vector3D losSensor = new Vector3D(cellDir.getAngle1(), cellDir.getAngle2());
+        Vector3D losECI = sensorToInertial.transformVector(losSensor).normalize();
+
+        Vector3D sensorPosECI = sensorPV.getPosition();
+
+        double dr = (rMax - rMin) / (nBins - 1);
+        int litCount = 0;
+
+        for (int i = 0; i < nBins; i++) {
+            double range = rMin + i * dr;
+
+            // Compute Earth-centered position of this bin point
+            Vector3D pointECI = sensorPosECI.add(losECI.scalarMultiply(range));
+            double pointRange = pointECI.getNorm();
+
+            // Angular direction from Earth center to the point
+            AngularDirection earthToPoint = 
+                new AngularDirection(eci, new double[]{pointECI.getAlpha(), pointECI.getDelta()}, 
+                                     AngleType.RADEC, pointRange);
+
+            int shadowFlag = checkInEarthShadowConical(earthToPoint, date);
+            if (shadowFlag == 0) { // 0 = lit
+                litCount++;
+            }
+        }
+
+        return (double) litCount / nBins;
+    }
+
+    private static int checkInEarthShadowConical(AngularDirection earthToPoint, AbsoluteDate date) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkInEarthShadowConical'");
+    }
+
+    protected static double computeChanceForDetection(TopocentricFrame sensorFrame,
+                                                      AbsoluteDate current,
+                                                      double azCenter,
+                                                      double elCenter,
+                                                      AngularDirection sunDir,
+                                                      AngularDirection moonDir) {
+
+        AngularDirection azEl = new AngularDirection(sensorFrame,
+                new double[]{azCenter, elCenter}, AngleType.AZEL, 1.);
+
+        // Distance from Moon direction
+        double actualDist = azEl.getEnclosedAngle(moonDir);
+        double weightMoon = (actualDist > TrackingObjective.minMoonDist) ? 1.0 : 0.0;
+
+        // Solar phase weight
+        double phiMod = sunDir.getEnclosedAngle(azEl);
+        double weightSolarPhase = (FastMath.sin(phiMod) - phiMod * FastMath.cos(phiMod)) / phiMod;
+
+        // Airmass weight
+        double airmass = 1. / (FastMath.sin(elCenter)
+                + 0.025 * FastMath.exp(-11 * FastMath.sin(elCenter)));
+        double airmassMax = 1. / (FastMath.sin(0.)
+                + 0.025 * FastMath.exp(-11 * FastMath.sin(0.)));
+        double weightAirmass = (1. / airmass - 1. / airmassMax) / (1 - 1. / airmassMax);
+
+        return weightMoon * weightSolarPhase * weightAirmass;
+    }
+
+    protected static double computeFoVCellReward(Fov fov,
+                                                 TopocentricFrame sensorFrame,
+                                                 PVCoordinates sensorPV,
+                                                 AbsoluteDate current,
+                                                 AngularDirection sunDir,
+                                                 AngularDirection moonDir,
+                                                 double rMin,
+                                                 double rMax,
+                                                 int nBins,
+                                                 Transform sensorToInertial) {
+
+        // Visibility / photometric weight
+        double visibilityReward = computeChanceForDetection(sensorFrame, current, 
+                                                            fov.getAzCenter(), 
+                                                            fov.getElCenter(), sunDir, 
+                                                            moonDir);
+
+        // Shadow / illumination weight
+        AngularDirection cellDir = 
+            new AngularDirection(sensorFrame, 
+                                 new double[]{fov.getAzCenter(), fov.getElCenter()},
+                                 AngleType.AZEL, 1.);
+
+        double shadowReward = computeShadowRewardFromSensor(sensorPV, cellDir, current, rMin, rMax, 
+                                                            nBins, sensorToInertial);
+
+        // Combine (product by default, can use weights if desired)
+        return visibilityReward * shadowReward;
     }
 }
